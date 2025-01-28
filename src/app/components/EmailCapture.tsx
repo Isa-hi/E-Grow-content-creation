2927e6decf8e7d8d59d44566a137bb4f2086dcf3
Héctor Isahi Ochoa <lacasademiamigochanel@gmail.com>
2025-01-27 22:31:57 -0600

    Refactor EmailCapture component to dynamically load Kajabi CSS and update form submission handling

--- src/app/components/EmailCapture.tsx
+++ src/app/components/EmailCapture.tsx
@@ -7,76 +7,54 @@ export default function EmailCapture() {
   const [email, setEmail] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState("");
-  const [isKajabiLoaded, setIsKajabiLoaded] = useState(false);
-  const [kajabiError, setKajabiError] = useState("");
 
-  // Cargar el script de Kajabi
   useEffect(() => {
-    if (!isKajabiLoaded) {
-      const script = document.createElement("script");
-      script.src = "https://decibel.mykajabi.com/forms/2148934872/embed.js";
-      script.async = true;
-      script.onload = () => setIsKajabiLoaded(true);
-      script.onerror = () =>
-        setKajabiError("Error al cargar el script de Kajabi.");
-      document.body.appendChild(script);
+    // Dynamically load Kajabi CSS
+    const cssLink = document.createElement("link");
+    cssLink.href =
+      "https://kajabi-app-assets.kajabi-cdn.com/assets/form_embed-dcaa78bdba8533a7a03bae89673cff66d373fa1b29f956e2e902fa95b731fbe9.css";
+    cssLink.rel = "stylesheet";
+    cssLink.type = "text/css";
+    document.head.appendChild(cssLink);
 
-      return () => {
-        document.body.removeChild(script);
-      };
-    }
-  }, [isKajabiLoaded]);
+    return () => {
+      document.head.removeChild(cssLink);
+    };
+  }, []);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsLoading(true);
     setMessage("");
 
     try {
-      // Usar el script de Kajabi para enviar el email
-      const form = document.querySelector(
-        "form[data-form-id='2148934872']"
-      ) as HTMLFormElement;
-
-      if (form) {
-        // Actualizar inputs con los valores actuales
-        const emailInput = form.querySelector(
-          "input[name='form_submission[name]']"
-        );
-        const nameInput = form.querySelector(
-          "input[name='form_submission[email]']"
-        );
-
-        if (emailInput && nameInput) {
-          (emailInput as HTMLInputElement).value = email;
-          (nameInput as HTMLInputElement).value = name;
-
-          console.log("Datos enviados a Kajabi:", { name, email });
-
-          // Simular el envío del formulario para que lo procese el script de Kajabi
-          setTimeout(() => {
-            form.submit(); // Esto permite que el script gestione los datos correctamente
-          }, 1000);
-        } else {
-          throw new Error("No se encontraron los inputs en el formulario.");
+      const response = await fetch(
+        "https://decibel.mykajabi.com/forms/2148934872/form_submissions",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: new URLSearchParams({
+            "form_submission[name]": name,
+            "form_submission[email]": email,
+          }).toString(),
         }
-      } else {
-        throw new Error("El formulario de Kajabi no se cargó correctamente.");
-      }
-
-      setMessage(
-        "¡Gracias por suscribirte! Revisa tu correo para más información."
       );
-      setEmail("");
-      setName("");
-    } catch (error: unknown) {
-      if (error instanceof Error) {
+
+      if (response.ok) {
         setMessage(
-          `Hubo un error al procesar tu suscripción: ${error.message}`
+          "¡Gracias por suscribirte! Revisa tu correo para más información."
         );
+        setEmail("");
+        setName("");
       } else {
-        setMessage("Hubo un error desconocido al procesar tu suscripción.");
+        setMessage("Hubo un error al procesar tu suscripción.");
       }
+    } catch (error) {
+      setMessage(
+        "Hubo un error desconocido al procesar tu suscripción. Inténtalo de nuevo más tarde."
+      );
     } finally {
       setIsLoading(false);
     }
@@ -95,55 +73,45 @@ export default function EmailCapture() {
           Suscríbete y recibe acceso inmediato a recursos gratuitos para crear
           contenido viral y lleva tu presencia online al siguiente nivel
         </p>
-        {!isKajabiLoaded && !kajabiError && (
-          <p className="text-center text-sm text-gray-300">
-            Cargando formulario...
-          </p>
-        )}
-        {kajabiError && (
-          <p className="text-center text-sm text-red-500">{kajabiError}</p>
-        )}
-        {isKajabiLoaded && (
-          <form
-            onSubmit={handleSubmit}
-            className="max-w-md mx-auto"
-            data-form-id="2148934872"
-          >
-            <fieldset>
-              <div className="text-field kajabi-form__form-item">
-                <input
-                  type="text"
-                  name="form_submission[name]"
-                  id="form_submission_name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                  placeholder="Name"
-                  className="appearance-none bg-transparent border-none w-full text-white leading-tight text-xl focus:outline-none"
-                />
-              </div>
-              <div className="email-field kajabi-form__form-item mt-4">
-                <input
-                  type="email"
-                  name="form_submission[email]"
-                  id="form_submission_email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  placeholder="Email"
-                  className="appearance-none bg-transparent border-none w-full text-white leading-tight text-xl focus:outline-none"
-                />
-              </div>
-              <button
-                type="submit"
-                className="kajabi-form__btn kajabi-form__btn--block-mobile mt-6 bg-gradient-to-r from-indigo-500 to-blue-500 hover:from-indigo-600 hover:to-green-600 border-indigo-500 hover:border-indigo-500 text-sm border-4 text-white py-1 px-2 rounded-full transition-all duration-300 flex items-center"
-                disabled={isLoading}
-              >
-                {isLoading ? "Enviando..." : "¡Obtener acceso!"}
-              </button>
-            </fieldset>
-          </form>
-        )}
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-md mx-auto"
+          data-form-id="2148934872"
+        >
+          <fieldset>
+            <div className="text-field kajabi-form__form-item">
+              <input
+                type="text"
+                name="form_submission[name]"
+                id="form_submission_name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                placeholder="Name"
+                className="appearance-none bg-transparent border-none w-full text-white leading-tight text-xl focus:outline-none"
+              />
+            </div>
+            <div className="email-field kajabi-form__form-item mt-4">
+              <input
+                type="email"
+                name="form_submission[email]"
+                id="form_submission_email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder="Email"
+                className="appearance-none bg-transparent border-none w-full text-white leading-tight text-xl focus:outline-none"
+              />
+            </div>
+            <button
+              type="submit"
+              className="kajabi-form__btn kajabi-form__btn--block-mobile mt-6 bg-gradient-to-r from-indigo-500 to-blue-500 hover:from-indigo-600 hover:to-green-600 border-indigo-500 hover:border-indigo-500 text-sm border-4 text-white py-1 px-2 rounded-full transition-all duration-300 flex items-center"
+              disabled={isLoading}
+            >
+              {isLoading ? "Enviando..." : "¡Obtener acceso!"}
+            </button>
+          </fieldset>
+        </form>
         {message && (
           <p className="mt-4 text-center text-sm text-gray-300">{message}</p>
         )}
